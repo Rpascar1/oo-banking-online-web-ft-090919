ffa061897f2b82d44c324895d5381d2e23140ca6
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-24 19:00:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   def execute_transaction
     counter = 0
     if sender.valid? && receiver.valid?
-      if amount <= sender.balance
+      if self.amount <= sender.balance
         receiver.balance += sender.amount && sender.balance -= sender.amount
         puts "complete"
      else
