4f3a9038ef7171ce9192f1e3eba25cf06fe86ced
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-24 18:13:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
 
     attr_reader :transfer, 
-    attr_accessor :reciever, :status,:sender
+    attr_accessor :reciever, :status, :sender
   def initialize(transfer, reciever)
     @transfer = transfer
     @reciever = reciever
