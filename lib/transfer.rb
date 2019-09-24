099931fa4d58c0bdd3ced86cbf0e8de26aef64c2
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-24 20:20:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
 
   def execute_transaction
            if valid? && sender.balance > amount 
-              receiver.balance+= self.amount && sender.balance -= amount
+              receiver.balance+= amount && sender.balance -= amount
               @status = "complete"
            else
               @status = "rejected"
